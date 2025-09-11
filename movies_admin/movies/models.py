48a1d0fe79069a249cb94fcc7bf1e3f144833d7c
Korochkin Stepan <stepa.stepa444@gmail.com>
2025-09-12 00:48:47 +0300

    progress on load_sql_data, added classes for load and save

--- movies_admin/movies/models.py
+++ movies_admin/movies/models.py
@@ -23,8 +23,8 @@ class Genre(UUIDMixin, TimeStampedMixin):
     def __str__(self):
         return self.name
 
-    name = models.CharField(_("name"), max_length=255)
-    description = models.TextField(_("description"), blank=True)
+    name = models.TextField(_("name"), null=False)
+    description = models.TextField(_("description"), blank=True, null=True)
 
     class Meta:
         db_table = 'content"."genre'
@@ -36,7 +36,7 @@ class Person(UUIDMixin, TimeStampedMixin):
     def __str__(self):
         return self.full_name
 
-    full_name = models.CharField(_("full_name"), max_length=255)
+    full_name = models.TextField(_("full_name"), null=False)
 
     class Meta:
         db_table = 'content"."person'
@@ -49,16 +49,16 @@ class FilmWork(UUIDMixin, TimeStampedMixin):
         return self.title
 
     MIN_LIMIT = MinValueValidator(limit_value=0)
-    MAX_LIMIT = MaxValueValidator(limit_value=100)
+    MAX_LIMIT = MaxValueValidator(limit_value=10)
     TYPE_CHOICES = [("movie", _("film")), ("tv_show", _("tv show"))]
 
-    title = models.CharField(_("title"), max_length=255)
-    description = models.TextField(_("description"), blank=True)
+    title = models.TextField(_("title"), null=False)
+    description = models.TextField(_("description"), blank=True, null=True)
     creation_date = models.DateField(_("creation_date"), blank=True, null=True)
     rating = models.FloatField(
         _("rating"), validators=[MIN_LIMIT, MAX_LIMIT], blank=True
     )
-    type = models.CharField(_("type"), choices=TYPE_CHOICES)
+    type = models.CharField(_("type"), choices=TYPE_CHOICES, null=False)
     genres = models.ManyToManyField(Genre, through="GenreFilmWork")
 
     class Meta:
@@ -72,7 +72,7 @@ class GenreFilmWork(UUIDMixin):
         "FilmWork", db_column="film_work_id", on_delete=models.CASCADE
     )
     genre = models.ForeignKey("Genre", db_column="genre_id", on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'content"."genre_film_work'
@@ -81,8 +81,8 @@ class GenreFilmWork(UUIDMixin):
 class PersonFilmWork(UUIDMixin):
     film_work = models.ForeignKey("FilmWork", on_delete=models.CASCADE)
     person = models.ForeignKey("Person", on_delete=models.CASCADE)
-    role = models.TextField(_("role"))
-    created = models.DateTimeField(auto_now_add=True)
+    role = models.TextField(_("role"), null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'content"."person_film_work'
--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -4,6 +4,19 @@ import psycopg
 from psycopg import ClientCursor, connection as _connection
 from psycopg.rows import dict_row
 
+class PostgresSaver:
+    def __init__(self, pg_connection):
+        self.connection = pg_connection
+
+    def save_all_data(self, data):
+        pass
+
+class SQLiteLoader:
+    def __init__(self, sql_connection):
+        self.connection = sql_connection
+
+    def load_movies(self):
+        pass
 
 def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
     """Основной метод загрузки данных из SQLite в Postgres"""
@@ -14,9 +27,15 @@ def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
     # postgres_saver.save_all_data(data)
 
 
-if __name__ == '__main__':
-    dsl = {'dbname': 'movies_database', 'user': 'app', 'password': '123qwe', 'host': '127.0.0.1', 'port': 5432}
-    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg.connect(
+if __name__ == "__main__":
+    dsl = {
+        "dbname": "movies_database",
+        "user": "app",
+        "password": "123qwe",
+        "host": "127.0.0.1",
+        "port": 5432,
+    }
+    with sqlite3.connect("db.sqlite") as sqlite_conn, psycopg.connect(
         **dsl, row_factory=dict_row, cursor_factory=ClientCursor
     ) as pg_conn:
         load_from_sqlite(sqlite_conn, pg_conn)
